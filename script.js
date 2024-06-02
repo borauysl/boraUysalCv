26b179288e7bf7f8dc579e6c9652e4988fd9e2c0
Bora <borauysll@gmail.com>
2024-06-02 16:09:48 +0300

    .

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function updateSkillProgress(skill, percent) {
     const progressBar = document.getElementById(skill + 'Progress').querySelector('div');
     
-    // Mevcut genişlik
+    
     let currentWidth = parseInt(progressBar.style.width, 10) || 0;
 
-    // Hedef genişlik
+   
     const targetWidth = percent;
 
-    // Animasyon hızı (milisaniye cinsinden)
+   
     const animationSpeed = 10; 
 
-    // Hedef genişliğe ulaşana kadar animasyonla genişliği artır
+    // hedef genişliğe ulaşana kadar animasyonla genişliği artır
     function animate() {
         if (currentWidth < targetWidth) {
             currentWidth++;
@@ -22,8 +22,8 @@ function updateSkillProgress(skill, percent) {
     animate();
 }
 
-// Her beceri için belirli beceri seviyelerini belirli aralıklarla güncelleyin
-updateSkillProgress('html', 80); // Html becerisi için yüzde 80'lik bir seviye
+// beceri puanları
+updateSkillProgress('html', 80); // html becerisi %80 örnek olarak
 updateSkillProgress('css', 60);
 updateSkillProgress('csharp', 85);
 updateSkillProgress('c', 75);
@@ -33,42 +33,42 @@ updateSkillProgress('mysql', 85);
 
 document.querySelectorAll('.container .left_side, .container .right_side').forEach(element => {
     element.addEventListener('mouseover', function() {
-        // Fare öğenin üzerine geldiğinde boyutunu veya rengini değiştirin
-        element.style.transform = 'scale(1.1)'; // Öğe boyutunu büyüt
-        element.style.transition = 'transform 0.3s ease'; // Animasyon süresi ve geçiş türü
+        // imleçle üstüne gelince 
+        element.style.transform = 'scale(1.1)'; // büyüt
+        element.style.transition = 'transform 0.3s ease'; // animasyon süresi
     });
 
     element.addEventListener('mouseout', function() {
-        // Fare öğenin üzerinden çıktığında boyutunu veya rengini eski haline getirin
-        element.style.transform = 'scale(1)'; // Öğe boyutunu normal boyuta döndür
+        // imleç üzerinden çıkınca
+        element.style.transform = 'scale(1)'; // boyutu normale çevir
     });
 });
 
-// Arka plan rengini ve sol kutunun rengini değiştiren bir animasyon
-let backgroundColors = ["rgb(161, 194, 205)", "#003147"]; // Arka planın geçiş yapacağı renkler
+// arkaplan rengi değiştirme
+let backgroundColors = ["rgb(161, 194, 205)", "#003147"]; 
 let index = 0; // Başlangıç renk indeksi
 
 function changeBackgroundAndBoxColor() {
     // Arka plan rengini değiştir
-    document.body.style.transition = "background-color 2s ease"; // Animasyon süresi ve geçiş türü
-    document.body.style.backgroundColor = backgroundColors[index]; // Arka plan rengini değiştir
+    document.body.style.transition = "background-color 2s ease"; 
+    document.body.style.backgroundColor = backgroundColors[index]; 
 
     let leftSide = document.querySelector('.container .left_side');
-    leftSide.style.transition = "background-color 2s ease"; // Animasyon süresi ve geçiş türü
+    leftSide.style.transition = "background-color 2s ease"; 
 
-    // Sonraki renge geçmek için indeksi arttır
+  
     index++;
     if (index >= backgroundColors.length) {
-        index = 0; // Renklerin sonuna gelindiğinde başa dön
+        index = 0; 
     }
 
-    // Sol kutunun rengini aynı anda arka plan rengiyle değiştir
+    
     setTimeout(function() {
-        leftSide.style.backgroundColor = backgroundColors[index]; // Sol kutunun rengini değiştir
-    }, 0); // Sol kutunun rengini 0 milisaniye geciktirerek aynı anda değiştirmek için
+        leftSide.style.backgroundColor = backgroundColors[index];
+    }, 0); 
 }
 
-// Belirli bir süre aralığıyla arka plan rengini ve sol kutunun rengini değiştirmek için bir zamanlayıcı kullan
+
 setInterval(changeBackgroundAndBoxColor, 5000); 
-// Belirli bir süre aralığıyla arka plan rengini ve sol kutunun rengini değiştirmek için bir zamanlayıcı kullan
- // 10000 milisaniye (10 saniye) aralıklarla arka plan rengini ve sol kutunun rengini değiştir
+
+ 
